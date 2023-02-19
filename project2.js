f1c158e282b0ed1807c8225247de35917d873b52
Cowboy <rishigupta12034@gmail.com>
2023-02-19 16:21:11 +0530

    Added more Methods

--- project2.js
+++ project2.js
@@ -1,21 +1,41 @@
-let color = function set(_read, next) {
-    setTimeout( function() {
-    document.body.style.backgroundColor = _read;
-    console.log(`${_read}`);
-    next && next(); 
-}, 3000)
-}
-
-color('red', 
-    color('green',
-        color('purple',
-            color('blue', ))));
-
-// function set(_colour) {
-//     setTimeout(function() {
-//         console.log("hello");
-//         document.body.style.backgroundColor = _colour;
-//     }, 3000);
+// Method 1(callback hell):
+// let color = function set(_color, next) {
+//         setTimeout( function() {
+//         document.body.style.backgroundColor = _color;
+//         console.log(`${_color}`);
+//         next && next(); 
+//     }, 1000)
 // }
 
-// set('red');
+// color('red', function() {
+//     color('green', function() {
+//         color('purple', function() {
+//             color('blue', () => {})
+//         })
+//     })
+// });
+
+// Method 2(using promises):
+let color1 = function set(_color, _delay) {
+    return new Promise( function(resolve, reject) {
+        setTimeout(() => {
+            document.body.style.backgroundColor = _color;
+            resolve();
+        }, 1000)
+    })
+}
+
+color1('red', 1000)
+    .then(() => {return color1('green', 1000)})         // can be written as: .then(() => color1('green', 1000))
+    .then(() => color1('purple', 1000))
+    .then(() => color1('blue', 1000))
+    .catch(() => reject());
+
+// Method 3(using async and await):
+async function colors() {
+    await color1('red', 1000);
+    await color1('green', 1000);
+    await color1('purple', 1000);
+    return "All Done";
+}
+colors();
